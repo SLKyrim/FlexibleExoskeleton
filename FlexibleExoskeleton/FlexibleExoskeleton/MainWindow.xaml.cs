b55de0cd2c874be2bb2c895b09cbd443749c6c12
Single Long <623962644@qq.com>
2019-06-14 15:09:48 +0800

    添加C#状态栏

--- FlexibleExoskeleton/FlexibleExoskeleton/MainWindow.xaml.cs
+++ FlexibleExoskeleton/FlexibleExoskeleton/MainWindow.xaml.cs
@@ -12,6 +12,10 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.IO.Ports;
+using System.IO;
+using System.Windows.Threading;
+using System.Xml.Serialization;
 
 namespace FlexibleExoskeleton
 {
@@ -20,9 +24,40 @@ namespace FlexibleExoskeleton
     /// </summary>
     public partial class MainWindow : Window
     {
-        public MainWindow()
+        #region 声明
+
+        #endregion
+
+        #region 界面初始化
+        private void Window_Loaded(object sender, RoutedEventArgs e)//打开程序窗口时执行
         {
-            InitializeComponent();
+            DispatcherTimer ShowTimer = new DispatcherTimer();
+            ShowTimer.Tick += new EventHandler(ShowCurTimer); //Tick是超过计时器间隔时发生事件，此处为Tick增加了一个叫ShowCurTimer的取当前时间并扫描串口的委托
+            ShowTimer.Interval = new TimeSpan(0, 0, 0, 1, 0); //设置刻度之间的时间值，设定为1秒（即文本框会1秒改变一次输出文本）
+            ShowTimer.Start();
+
+            //DispatcherTimer showTimer = new DispatcherTimer();
+            //showTimer.Tick += new EventHandler(ShowSenderTimer); //增加了一个叫ShowSenderTimer的在电机和传感器的只读文本框中输出信息的委托
+            //showTimer.Interval = new TimeSpan(0, 0, 0, 0, 100);  //文本变化间隔是??毫秒(并不准确)
+            //showTimer.Start();
         }
+
+        public void ShowCurTimer(object sender, EventArgs e)//取当前时间并扫描可用串口的委托
+        {
+            string timeDateString = "";
+            DateTime now = DateTime.Now;
+            timeDateString = string.Format("{0}年{1}月{2}日 {3}:{4}:{5}",
+                now.Year,
+                now.Month.ToString("00"),
+                now.Day.ToString("00"),
+                now.Hour.ToString("00"),
+                now.Minute.ToString("00"),
+                now.Second.ToString("00"));
+
+            timeDateTextBlock.Text = timeDateString;
+
+            //ScanPorts();//扫描可用串口
+        }
+        #endregion
     }
 }
