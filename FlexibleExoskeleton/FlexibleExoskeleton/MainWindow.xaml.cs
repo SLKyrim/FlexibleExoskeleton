cb25a71c1cb06476756c31be83c54a38c8359d68
Single Long <623962644@qq.com>
2019-06-14 17:06:07 +0800

    添加选择串口功能
    串口数据处理还需要接洽

--- FlexibleExoskeleton/FlexibleExoskeleton/MainWindow.xaml.cs
+++ FlexibleExoskeleton/FlexibleExoskeleton/MainWindow.xaml.cs
@@ -25,7 +25,14 @@ namespace FlexibleExoskeleton
     public partial class MainWindow : Window
     {
         #region 声明
+        // 声明串口类实例
+        private Ports ports = new Ports();
 
+        //扫描串口
+        private string[] SPCount = null; //用来存储计算机串口名称数组
+        private int comcount = 0; //用来存储计算机可用串口数目，初始化为0
+        private bool flag = false;
+        private string readData_com = null; //存储读取数据所用串口
         #endregion
 
         #region 界面初始化
@@ -42,7 +49,7 @@ namespace FlexibleExoskeleton
             //showTimer.Start();
         }
 
-        public void ShowCurTimer(object sender, EventArgs e)//取当前时间并扫描可用串口的委托
+        private void ShowCurTimer(object sender, EventArgs e)//取当前时间并扫描可用串口的委托
         {
             string timeDateString = "";
             DateTime now = DateTime.Now;
@@ -56,8 +63,88 @@ namespace FlexibleExoskeleton
 
             timeDateTextBlock.Text = timeDateString;
 
-            //ScanPorts();//扫描可用串口
+            ScanPorts();//扫描可用串口
         }
         #endregion
+
+        #region 串口扫描
+        private void ScanPorts()//扫描可用串口
+        {
+            SPCount = ports.CheckSerialPortCount(); //获得计算机可用串口名称数组
+
+            ComboBoxItem tempComboBoxItem = new ComboBoxItem();
+
+            if (comcount != SPCount.Length) //SPCount.length其实就是可用串口的个数
+            {
+                //当可用串口计数器与实际可用串口个数不相符时
+                //初始化串口选择下拉窗口并将flag初始化为false
+                DataComboBox.Items.Clear();
+
+                tempComboBoxItem = new ComboBoxItem();
+                tempComboBoxItem.Content = "请选择端口";
+                DataComboBox.Items.Add(tempComboBoxItem);
+                DataComboBox.SelectedIndex = 0;
+
+                readData_com = null;
+
+                flag = false;
+
+                if (comcount != 0)
+                {
+                    //在操作过程中增加或减少串口时发生
+                    //MessageBox.Show("串口数目已改变，请重新选择串口");
+                    statusBar.Background = new SolidColorBrush(Color.FromArgb(255, 230, 20, 20));
+                    statusInfoTextBlock.Text = "端口数目已改变，请重新选择端口！";
+                }
+
+                comcount = SPCount.Length;     //将可用串口计数器与现在可用串口个数匹配
+            }
+
+            if (!flag)
+            {
+                if (SPCount.Length > 0) //有可用串口时执行
+                {
+
+                    comcount = SPCount.Length;
+
+                    statusBar.Background = new SolidColorBrush(Color.FromArgb(255, 0, 122, 204));
+                    statusInfoTextBlock.Text = "检测到" + SPCount.Length + "个端口!";
+
+                    for (int i = 0; i < SPCount.Length; i++)
+                    {
+                        //分别将可用串口添加到三个下拉窗口中
+                        string tempstr = SPCount[i];
+
+                        tempComboBoxItem = new ComboBoxItem();
+                        tempComboBoxItem.Content = tempstr;
+                        DataComboBox.Items.Add(tempComboBoxItem);
+                    }
+
+                    flag = true;
+                }
+                else
+                {
+                    comcount = 0;
+                    statusBar.Background = new SolidColorBrush(Color.FromArgb(255, 230, 20, 20));
+                    statusInfoTextBlock.Text = "未检测到端口!";
+                }
+            }
+        }
+        #endregion
+
+        private void DataComboBox_DropDownClosed(object sender, EventArgs e) // 【端口号】下拉选项框选择好后执行
+        {
+            ComboBoxItem item = DataComboBox.SelectedItem as ComboBoxItem; //下拉窗口当前选中的项赋给item
+            string tempstr = item.Content.ToString(); //将选中的项目转为字串存储在tempstr中
+
+            for (int i = 0; i < SPCount.Length; i++)
+            {
+                if (tempstr == SPCount[i])
+                {
+                    readData_com = SPCount[i];
+                    ports.Data_SerialPort_Init(SPCount[i]); //串口初始化
+                }
+            }
+        }
     }
 }
---
+++ FlexibleExoskeleton/FlexibleExoskeleton/Ports.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.IO.Ports;
+using System.Windows.Threading;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace FlexibleExoskeleton
+{
+    // 集成串口功能的类
+    class Ports
+    {
+        #region 声明
+        //获取可用串口名
+        private string[] IsOpenSerialPortCount = null;
+
+        //声明串口实例
+        private SerialPort data_SerialPort = new SerialPort(); //【读取数据】串口
+        #endregion
+
+        #region 扫描串口
+        public string[] CheckSerialPortCount()//扫描本地计算机获取可用串口名
+        {
+            IsOpenSerialPortCount = SerialPort.GetPortNames();
+            return IsOpenSerialPortCount;
+        }
+        #endregion
+
+        #region 串口初始化
+        public void Data_SerialPort_Init(string comstring)//【读取数据】串口初始化
+        {
+            if (data_SerialPort != null) //换端口号时执行
+            {
+                if (data_SerialPort.IsOpen)
+                {
+                    data_SerialPort.Close(); // 若串口已占用，则先关闭串口
+                }
+            }
+
+            data_SerialPort = new SerialPort(); // 串口实例
+            data_SerialPort.PortName = comstring; // 串口名
+            data_SerialPort.BaudRate = 115200; // 比特率
+            //data_SerialPort.DataBits = 8; // 数据位
+            data_SerialPort.Parity = Parity.None; // 校检位
+            data_SerialPort.StopBits = StopBits.One; // 停止位
+            data_SerialPort.Open();
+            data_SerialPort.DataReceived += new System.IO.Ports.SerialDataReceivedEventHandler(DataReceived);
+        }
+        #endregion
+
+        #region 串口通信接收下位机数据
+        private void DataReceived(object sender, SerialDataReceivedEventArgs e)//【读取数据】串口接收数据（算法）
+        {
+            try
+            {
+                int bufferlen = data_SerialPort.BytesToRead; //先记录下来，避免某种原因，人为的原因，操作几次之间时间长，缓存不一致
+                if (bufferlen >= 27) //一个电机有使能，方向，转速，电流4个参数，前两个各占1个字节（2个十六进制位），后两个各占2个字节，故一个电机数据占6个字节，加上一个开始位，两个停止位，故总有1+6*4+2=27字节
+                {
+
+                }
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("端口读取数据失败！");
+            }
+        }
+        #endregion
+    }
+}
